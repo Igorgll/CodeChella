6687376ce750f1d42130b3f1b239ebf92620e191
igorgll <igorlimagn@gmail.com>
2023-03-07 15:40:02 -0300

    @igorgll [Dates e footer]

--- script/script.js
+++ script/script.js
@@ -5,3 +5,24 @@ function changeNavButton() {
   button.classList.toggle("active");
   nav_items.classList.toggle("active");
 }
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const sabado__bandas = data.sabado;
+    const domingo__bandas = data.domingo;
+
+    const sabado_list = document.getElementById("sabado_list");
+    sabado__bandas.forEach((sabado) => {
+      const li = document.createElement("li");
+      li.textContent = sabado;
+      sabado_list.appendChild(li);
+    });
+
+    const domingo_list = document.getElementById("domingo_list");
+    domingo__bandas.forEach((domingo) => {
+      const li = document.createElement("li");
+      li.textContent = domingo;
+      domingo_list.appendChild(li);
+    });
+  });
