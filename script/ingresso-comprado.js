18a731d0819dad7728b640f51825430458638ef7
igorgll <igorlimagn@gmail.com>
2023-03-23 15:18:23 -0300

    @igorgll [Página ingresso comprado]

---
+++ script/ingresso-comprado.js
@@ -0,0 +1,6 @@
+import changeNavButton from "./index.js";
+
+const changeNavButtonIngressoComprado = document.querySelector(".btn_nav");
+changeNavButtonIngressoComprado.onclick = () => {
+  changeNavButton();
+};
--- script/ingresso.js
+++ script/ingresso.js
@@ -1,2 +1,9 @@
+import changeNavButton from "./index.js";
+
 const paginaAtiva = document.getElementById("pagina_ativa");
 paginaAtiva.style.textDecoration = "underline";
+
+const changeNavButtonIngresso = document.querySelector(".btn_nav");
+changeNavButtonIngresso.onclick = () => {
+  changeNavButton();
+};
