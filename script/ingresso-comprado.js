d8bbb55b225a1762a03e42380e4e1201fbc14bc3
igorgll <igorlimagn@gmail.com>
2023-03-25 16:46:03 -0300

    @igorgll [Boreal Theme Página Ingresso Comprado]

--- script/ingresso-comprado.js
+++ script/ingresso-comprado.js
@@ -1,4 +1,9 @@
 import changeNavButton from "./index.js";
+import { changeNavbarBackground } from "./index.js";
+import { changeFontColor } from "./index.js";
+import { changeFooterBackground } from "./index.js";
+import { changeFooterLogoColor } from "./index.js";
+import { changeSocialIconsColor } from "./index.js";
 
 const paginaAtiva = document.getElementById("pagina_ativa");
 paginaAtiva.style.textDecoration = "underline";
@@ -7,3 +12,46 @@ const changeNavButtonIngressoComprado = document.querySelector(".btn_nav");
 changeNavButtonIngressoComprado.onclick = () => {
   changeNavButton();
 };
+
+const changeThemeIngressoComprado = document.getElementById(
+  "toggleSwitchIngressoComprado"
+);
+changeThemeIngressoComprado.onclick = () => {
+  changeThemeToBorealIngressoComprado();
+};
+
+function changeBackgroundGradientIngressoComprado() {
+  const ingressoCompradoBg = document.querySelector(".section_ingresso");
+  ingressoCompradoBg.classList.toggle("active");
+}
+
+function changeIngressoCompradoBackground() {
+  const ingressoBg = document.querySelector(".ingresso_qrcode");
+  ingressoBg.classList.toggle("active");
+}
+
+function changeQrCodeIngressoLogo() {
+  const logoIngresso = document.getElementById("logo_ingresso");
+  if (logoIngresso.src.includes("/assets/Logo1.svg")) {
+    logoIngresso.src = "/assets/logo-ingresso-boreal.svg";
+  } else {
+    logoIngresso.src = "/assets/Logo1.svg";
+  }
+}
+
+function changeBannerBorealIngressoComprado() {
+  const banner_ingresso_comprado = document.querySelector(".banner_ingresso-comprado");
+  banner_ingresso_comprado.classList.toggle("banner_ingresso_comprado-boreal");
+}
+
+function changeThemeToBorealIngressoComprado() {
+  changeFontColor();
+  changeNavbarBackground();
+  changeBannerBorealIngressoComprado()
+  changeBackgroundGradientIngressoComprado();
+  changeQrCodeIngressoLogo();
+  changeIngressoCompradoBackground();
+  changeFooterBackground();
+  changeSocialIconsColor();
+  changeFooterLogoColor();
+}
