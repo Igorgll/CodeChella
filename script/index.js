a3f69aebbc59450b50a85cc3a74406809c1c8a5a
igorgll <igorlimagn@gmail.com>
2023-03-31 22:57:13 -0300

    @igorgll [Ajustes troca de tema]

--- script/index.js
+++ script/index.js
@@ -2,6 +2,21 @@ import { setTheme } from "./experiencia.js";
 
 window.addEventListener("load", function () {
   setTheme();
+
+  const toggle = document.getElementById("toggleSwitch");
+  if (toggle) {
+    toggle.checked = localStorage.getItem("theme") === "boreal";
+    toggle.addEventListener("change", () => {
+      let targetTheme;
+      if (toggle.checked) {
+        targetTheme = "boreal";
+      } else {
+        targetTheme = "verao";
+      }
+      document.documentElement.setAttribute("data-theme", targetTheme);
+      localStorage.setItem("theme", targetTheme);
+    });
+  }
 });
 
 export default function changeNavButton() {
