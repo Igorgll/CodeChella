83f4beb6ca1c2401eb3849a4859bc01540e6eb05
igorgll <igorlimagn@gmail.com>
2023-04-01 20:04:00 -0300

    @igorgll [Animações]

--- script/informacoes.js
+++ script/informacoes.js
@@ -1,8 +1,10 @@
 import changeNavButton from "./index.js";
 import { setTheme } from "./experiencia.js";
 
+const first_content = document.querySelector(".conteudo_first");
 window.addEventListener("load", function () {
   setTheme();
+  first_content.classList.add("active");
 });
 
 const paginaAtiva = document.getElementById("pagina_ativa");
@@ -49,32 +51,18 @@ if (changeThemeMobile) {
   });
 }
 
-const first_question = document.querySelector(".first_question");
-const first_content = document.querySelector(".conteudo_first");
-first_question.onclick = () => {
-  first_content.classList.toggle("active");
-};
+const questions = document.querySelectorAll(".question");
+const contents = document.querySelectorAll(".conteudo");
 
-const second_question = document.querySelector(".second_question");
-const second_content = document.querySelector(".conteudo_second");
-second_question.onclick = () => {
-  second_content.classList.toggle("active");
-};
+function removeActiveClass() {
+  contents.forEach(content => content.classList.remove("active"));
+}
 
-const third_question = document.querySelector(".third_question");
-const third_content = document.querySelector(".conteudo_third");
-third_question.onclick = () => {
-  third_content.classList.toggle("active");
-};
-
-const fourth_question = document.querySelector(".fourth_question");
-const fourth_content = document.querySelector(".conteudo_fourth");
-fourth_question.onclick = () => {
-  fourth_content.classList.toggle("active");
-};
-
-const fifth_question = document.querySelector(".fifth_question");
-const fifth_content = document.querySelector(".conteudo_fifth");
-fifth_question.onclick = () => {
-  fifth_content.classList.toggle("active");
-};
+questions.forEach(question => {
+  question.onclick = () => {
+    if (!question.nextElementSibling.classList.contains("active")) {
+      removeActiveClass();
+      question.nextElementSibling.classList.add("active");
+    }
+  }
+});
